8bde50aaa4a51979f5a4caf55335f9d722f027c4
Akanksha <kmakankshakumar@gmail.com>
2022-03-23 16:38:47 +0530

    Time: 73 ms (70.24%), Space: 35.7 MB (43.90%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cs
+++ 771-jewels-and-stones/771-jewels-and-stones.cs
@@ -13,3 +13,13 @@ public class Solution {
         return count;
     }
 }
+
+/*public int numJewelsInStones(String jewels, String stones) {
+        int result = 0;
+        for(int i = 0; i < stones.length(); i++){
+            if(jewels.indexOf(stones.charAt(i)) != -1){
+                result++;
+            }
+        }
+        return result;
+    }*/
